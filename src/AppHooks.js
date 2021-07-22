cf9f453e0d64b99921fb70a44d2fc74307bb26a4
Fred <fmoore1124@gmail.com>
2021-07-22 02:50:24 -0400

    updating our app to redirect to home if user is signed in

--- src/AppHooks.js
+++ src/AppHooks.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
 import Header from './components/Header/Header';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/SignInAndSignUp';
+import { useSelector } from 'react-redux';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { useDispatch } from 'react-redux';
@@ -13,6 +14,7 @@ import userReducer from './redux/user/userReducer';
 
 const AppHooks = () => {
   const dispatch = useDispatch(userReducer);
+  const user = useSelector((state) => state.user.currentUser);
 
   useEffect(() => {
     const unsubscribeFromAuth = auth.onAuthStateChanged(async (authUser) => {
@@ -41,7 +43,11 @@ const AppHooks = () => {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUp} />
+        <Route
+          exact
+          path='/signin'
+          render={() => (user ? <Redirect to='/' /> : <SignInAndSignUp />)}
+        />
       </Switch>
     </div>
   );
